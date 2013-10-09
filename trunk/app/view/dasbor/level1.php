dba609356b85a25b1597d7970b688a61c43b1b39
nez817@gmail.com <nez817@gmail.com@d4d5b290-ae63-a6e4-1b15-69b755dbb522>
2013-10-09 08:19:56 +0000

    header, css, level1

--- trunk/app/view/dasbor/level1.php
+++ trunk/app/view/dasbor/level1.php
@@ -1,46 +1,25 @@
 <!doctype html>
-
-                        <?php                        
-						$sp2d= 0;
-                        foreach ($this->data as $val) {   
-                                                       
-							$sp2d+=$val->get_kd_d_sp2d();
-                        }
-                        ?>
+    <?php                        
+		$sp2d= 0;
+            foreach ($this->data as $val) {   
+				$sp2d+=$val->get_kd_d_sp2d();
+            }
+    ?>
 					
 		<div class="kolom1" id="g1" href="#">
 			<input type="hidden" id="sp2d" value= "<?php echo $sp2d ?>">
 		</div>
 	<div class="kolom2">
-	<table width="100%" style="margin: 0px; padding: 0px">
-		<tr>
-			<td><button><div id="g2"></div></button></td>
-			<td><button><div id="g3"></div></button></td>
-			<td><button><div id="g4"></div></button></td>
-			
-		</tr>
-		<tr>
-			<td><button><div id="g5"></div></button></td>
-			<td><button><div id="g6"></div></button></td>
-			<td><button><div id="g7"></div></button></td>
-			
-		</tr>
-		<tr>
-			<td><button><div id="g8"></div></button></td>
-			<td><button><div id="g9"></div></button></td>
-		</tr>
-	</table>
-		
-		
-		
+			<center>
+			<button><div id="g2"></div></button><br>
+			<button><div id="g3"></div></button>
+			</center>
 	</div>
 	
-
-
     <!--script-->
 
     <script>
-      var g1, g2, g3, g4, g5, g6, g7, g8, g9;
+      var g1, g2, g3;
       
       window.onload = function(){
         var jum_sp2d = document.getElementById('sp2d').value;
@@ -70,66 +49,11 @@
           title: "Kanwil",
           label: "oz"
         });
-        
-        var g4 = new JustGage({
-          id: "g4", 
-          value: getRandomInt(0, 100), 
-          min: 0,
-          max: 100,
-          title: "BA999",
-          label: "oz"
-        });
-		
-		var g5 = new JustGage({
-          id: "g5", 
-          value: getRandomInt(0, 100), 
-          min: 0,
-          max: 100,
-          title: "PKN",
-          label: "oz"
-        });
-		
-		var g6 = new JustGage({
-          id: "g6", 
-          value: getRandomInt(0, 100), 
-          min: 0,
-          max: 100,
-          title: "Little Pal",
-          label: "oz"
-        });
-		
-		var g7 = new JustGage({
-          id: "g7", 
-          value: getRandomInt(0, 100), 
-          min: 0,
-          max: 100,
-          title: "APK",
-          label: "oz"
-        });
-		
-		var g8 = new JustGage({
-          id: "g8", 
-          value: getRandomInt(0, 100), 
-          min: 0,
-          max: 100,
-          title: "SMI",
-          label: "oz"
-        });
-		
-		var g9 = new JustGage({
-          id: "g9", 
-          value: getRandomInt(0, 100), 
-          min: 0,
-          max: 100,
-          title: "DJPU",
-          label: "oz"
-        });
       
         setInterval(function() {
           //g1.refresh(getRandomInt(50, 100));
           g2.refresh(getRandomInt(50, 100));          
           g3.refresh(getRandomInt(0, 50));
-          g4.refresh(getRandomInt(0, 50));
         }, 2500);
       };
     </script>
