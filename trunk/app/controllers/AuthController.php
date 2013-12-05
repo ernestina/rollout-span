769a4405e0b258812893953f567e4a11a9a0b43f
mashtree@gmail.com <mashtree@gmail.com@d4d5b290-ae63-a6e4-1b15-69b755dbb522>
2013-12-05 10:35:04 +0000

    modifikasi class AuthController

--- trunk/app/controllers/AuthController.php
+++ trunk/app/controllers/AuthController.php
@@ -28,23 +28,7 @@ class AuthController extends BaseController {
             Session::set('user', $user);
             Session::set('role', $res[1]);
             Session::set('id_user',$res[3]);
-            if (Session::get('role') == ADMIN) {
-                header('location:' . URL);
-            } elseif (Session::get('role') == KPPN) {
-                //if (Session::get('user') == 'KPPN JAKARTA II') {
-                header('location:' . URL . 'dataKppn/addDataKppnLvl3');
-                //} elseif (Session::get('user') == 'KPPN JAKARTA VI') {
-                    //header('location:' . URL . 'dataKppn/addDataKppnLvl3Jkt6');
-                //} else {
-                    //header('location:' . URL . 'auth/login');
-                //}
-            } elseif (Session::get('role') == BA999) {
-                header('location:' . URL . 'dataBa/adddataBa');
-            } elseif (Session::get('role') == PKN) {
-                header('location:' . URL . 'dataPkn/addDataPkn');
-            } else {
-                header('location:' . URL . 'auth/login');
-            }
+            header('location:' . URL);
         } else if ((int) $res[0] == 0) {
             $this->view->error = "user tidak ditemukan!";
             $this->view->load('admin/login');
