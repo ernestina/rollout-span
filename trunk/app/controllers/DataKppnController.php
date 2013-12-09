5101a8bd7897e8f4689a32e471d3ad3909605df0
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@d4d5b290-ae63-a6e4-1b15-69b755dbb522>
2013-12-09 06:49:51 +0000

    ketinggalan model ama controller level 1 nya

--- trunk/app/controllers/DataKppnController.php
+++ trunk/app/controllers/DataKppnController.php
@@ -21,12 +21,25 @@ class DataKppnController extends BaseController {
     public function index() {
         $this->view->render('admin/dataKppnList');
     }
+	
+	/*
+     * view Data KPPN PER KANWIL
+     */
+
+    public function viewDataKppnLvl1() {
+        $d_kppn = new DataKppn($this->registry);
+        $d_bobot = new DataBobot($this->registry);
+        $this->view->bobot = $d_bobot->get_bobot_kppn_lvl3();
+        $this->view->dasbor = $d_kppn->get_d_kppn_per_tgl();
+        $this->view->data = $d_kppn->get_d_kppn_lvl1();
+        $this->view->render('admin/dataKppnLvl1');
+    }
 
     /*
      * view Data KPPN 
      */
 
-    public function viewDataKppnLvl2() {
+    public function viewDataKppnLvl2($id=null) {
         $d_kppn = new DataKppn($this->registry);
         $d_bobot = new DataBobot($this->registry);
         $this->view->bobot = $d_bobot->get_bobot_kppn_lvl3();
