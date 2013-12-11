63ced11d70a66bed5027364c16111f06fbaaf880
nez817@gmail.com <nez817@gmail.com@d4d5b290-ae63-a6e4-1b15-69b755dbb522>
2013-12-11 08:22:23 +0000

    edit footer developer

--- trunk/app/view/Footer.php
+++ trunk/app/view/Footer.php
@@ -1,5 +1,19 @@
 </div>
 </body>
 <hr>
-<center><footer>Direktorat Transformasi Perbendaharaan - &copy 2013 <a href="http://www.perbendaharaan.go.id">Developer</a></footer></center>
+<center><footer><h3>Direktorat Transformasi Perbendaharaan - &copy 2013. <a href="#openModal">Developer</a><h3>
+
+<div id="openModal" class="modalDialog" >
+	<div>
+		<h2>Developer :</h2>
+		<hr style="color: rgba(0,121,185,1); margin-top: -10px">
+		<a href="#close" title="Tutup" class="close"><i class="icon-remove icon-white" style="margin-left: 4px; margin-top: 5px"></i></a>
+		<h3>
+		<a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">1. Andi Saputra</a> - 19850911200710001 - KPPN Tanjung Pinang </br> 
+		<a href="mailto:mashtree@gmail.com" target="_top">2. Triyono</a> -  198205152001121001 - AKKP Setditjen</br>    
+		<a href="mailto:nez817@gmail.com" target="_top">3. Ernestina R</a> - 198401282003122001 - Kanwil Jawa Tengah</h3>
+		
+	</div>
+</div>
+</footer></center>
 </html>
