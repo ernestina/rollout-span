17de56bf04e806026cf8d03a1e693419ff27ccb3
mashtree@gmail.com <mashtree@gmail.com@d4d5b290-ae63-a6e4-1b15-69b755dbb522>
2013-12-12 02:46:19 +0000

    -- perbaikan

--- trunk/app/view/dasbor/kppnLvl1.php
+++ trunk/app/view/dasbor/kppnLvl1.php
@@ -17,23 +17,42 @@
     //var len_data = Object.keys(data_kanwil).length; 
     var label = new Array();
     var data_bar = new Array();
+    var min = 100;
     for(var i in data_kanwil){
         var tmp = Math.ceil(data_kanwil[i].sum);
         label.push(data_kanwil[i].singkat_kanwil);
         data_bar.push(tmp);
+        if(tmp<=min){
+            min = tmp;
+        }
     }
+    var mod = min%10;
+    min -= mod;
+    var step = (100-min)/10; 
     console.log(label);
     console.log(data_bar);
     var data = {
     labels : label,
     datasets : [
         {
-            fillColor : "rgba(151,187,205,0.5)",
-            strokeColor : "rgba(151,187,205,1)",
+            fillColor : "rgba(243,190,0,0.3)",
+            strokeColor : "rgba(243,190,0,1)",
+            pointColor : "rgba(243,190,0,1)",
+            pointStrokeColor : "#fff",
             data : data_bar
         }
     ]
 }
-    var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Bar(data);
+
+var options = {
+    scaleOverride : true,
+    // Number - The number of steps in a hard coded scale
+    scaleSteps : step,
+    // Number - The value jump in the hard coded scale
+    scaleStepWidth : 10,
+    // Number - The scale starting value
+    scaleStartValue : min,
+}
+    var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Bar(data, options);
 	
 </script>
