401a119f89cf94a3a8997afadc0d8e2b210c44b1
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@d4d5b290-ae63-a6e4-1b15-69b755dbb522>
2013-12-13 00:55:09 +0000

    memperbaiki perhitungan BA.999 dan PKN memperbaiki perhitungan level 1 memperbaiki footer dan modal developer

--- app/view/Footer.php
+++ app/view/Footer.php
@@ -9,7 +9,7 @@
 		<hr style="color: rgba(0,121,185,1); margin-top: -10px">
 		<a href="#close" title="Tutup" class="close"><i class="icon-remove icon-white" style="margin-left: 4px; margin-top: 5px"></i></a>
 		<h3>
-		<a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">1. Andi Saputra</a> - 19850911200710001 - KPPN Tanjung Pinang </br> 
+		<a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">1. Andi Saputra</a> - 198509112007101001 - KPPN Tanjung Pinang </br> 
 		<a href="mailto:mashtree@gmail.com" target="_top">2. Triyono</a> -  198205152001121001 - AKKP Setditjen</br>    
 		<a href="mailto:nez817@gmail.com" target="_top">3. Ernestina R</a> - 198401282003122001 - Kanwil Jawa Tengah</h3>
 		
